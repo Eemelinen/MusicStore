f7c57ea4ef964962566285ab4098067b855a0c91
Eemeli Surakka <37372229+Eemelinen@users.noreply.github.com>
2018-12-08 21:16:03 +0200

    Add files via upload
    8.12. Updates:
    Classes and basic Dao-functionality added.

---
+++ Musicstore/src/com/musicstore/web/jdbc/Album.java
@@ -0,0 +1,51 @@
+package com.musicstore.web.jdbc;
+
+public class Album {
+	
+	private long albumId;
+	private String albumTitle;
+	private long artistId;
+	
+	public Album(String albumTitle, long artistId) {
+		super();
+		this.albumTitle = albumTitle;
+		this.artistId = artistId;
+	}
+
+	public Album(long albumId, String albumTitle, long artistId) {
+		super();
+		this.albumId = albumId;
+		this.albumTitle = albumTitle;
+		this.artistId = artistId;
+	}
+
+	public long getAlbumId() {
+		return albumId;
+	}
+
+	public void setAlbumId(long albumId) {
+		this.albumId = albumId;
+	}
+
+	public String getAlbumTitle() {
+		return albumTitle;
+	}
+
+	public void setAlbumTitle(String albumTitle) {
+		this.albumTitle = albumTitle;
+	}
+
+	public long getArtistId() {
+		return artistId;
+	}
+
+	public void setArtistId(long artistId) {
+		this.artistId = artistId;
+	}
+
+	@Override
+	public String toString() {
+		return "Album [albumId=" + albumId + ", albumTitle=" + albumTitle + ", artistId=" + artistId + "]";
+	}
+
+}
