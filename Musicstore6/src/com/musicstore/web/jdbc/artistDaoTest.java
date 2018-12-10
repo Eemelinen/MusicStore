0e1c232fd413a406930dd01dacd643f36272459c
Eemeli Surakka <37372229+Eemelinen@users.noreply.github.com>
2018-12-10 02:45:27 +0200

    Add files via upload

---
+++ Musicstore6/src/com/musicstore/web/jdbc/artistDaoTest.java
@@ -0,0 +1,31 @@
+//package com.musicstore.web.jdbc;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertFalse;
+//
+//import java.sql.SQLException;
+//import java.util.List;
+//
+//import org.junit.jupiter.api.Test;
+//
+//public class artistDaoTest {
+//	
+//	@Test
+//	public void testArtistListIsNotEmpty() throws ClassNotFoundException, SQLException {
+//		
+//		ArtistDao dao = new ArtistDao();
+//		List<Artist> all = dao.getAllArtists();
+//		
+//		assertFalse(all.isEmpty());
+//	}
+//	
+//	@Test
+//	public void testTheNameOfTheFirstArtist() throws ClassNotFoundException, SQLException {
+//		
+//		ArtistDao dao = new ArtistDao();
+//		List<Artist> all = dao.getAllArtists();
+//		
+//		assertEquals("A Cor Do Som", all.get(0).getName());
+//	}
+//
+//}
---
+++ Musicstore6/src/com/musicstore/web/jdbc/sqliteTest.java
@@ -0,0 +1,40 @@
+package com.musicstore.web.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class sqliteTest {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		
+		String sql = "SELECT * FROM Album WHERE ArtistId = (SELECT ArtistId FROM Artist WHERE Name = ?)";
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		databaseConnection db = new databaseConnection();
+				
+		Connection conn = db.connect();
+		PreparedStatement statement = conn.prepareStatement(sql);
+		
+		System.out.print("Write Artist Name:");
+		
+		String artistName = scanner.nextLine();
+		
+		statement.setString(1, artistName);
+		ResultSet results = statement.executeQuery();
+		
+		System.out.println("Found albums:");
+		while(results.next()) {
+			
+			System.out.println(results.getString("Title"));
+			
+		}
+		
+		db.close(results, statement, conn);
+		scanner.close();
+	
+	}
+}
